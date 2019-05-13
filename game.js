e6ab0898c1e1a32bc974a637c58eaa81f4f35ade
Dustin VanCour <dustin82@gmail.com>
2019-05-13 01:47:13 -0600

    added P & K buttons

--- game.js
+++ game.js
@@ -3,8 +3,11 @@ let health = 100
 function slap() {
   health--
   console.log(health)
+  update()
 }
 
 function update() {
-  document.getElementById("health").innerText
+  document.getElementById("health").innerText = health
 }
+
+update()

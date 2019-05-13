ead0b020e1f54610951be0ae7742060d90b476fb
Dustin VanCour <dustin82@gmail.com>
2019-05-13 02:11:21 -0600

    Part 1 - DONE!

--- game.js
+++ game.js
@@ -1,13 +1,33 @@
+let name = 'Slappy the Clown'
 let health = 100
+let hits = 0
 
 function slap() {
   health--
+  hits++
+  console.log(health)
+  update()
+}
+
+function punch() {
+  health -= 5
+  hits++
+  console.log(health)
+  update()
+}
+
+function kick() {
+  health -= 10
+  hits++
   console.log(health)
   update()
 }
 
 function update() {
-  document.getElementById("health").innerText = health
+  document.getElementById("health").innerText = health;
+  document.getElementById("hit-counter").innerText = hits;
+  document.getElementById("name").innerText = name;
 }
+
 
 update()
